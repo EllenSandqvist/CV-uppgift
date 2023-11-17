1811b491e1fdde91b640ba6f1499455796f8cb68
Ellen Sandqvist <ell1sandqvist@gmail.com>
2023-11-17 08:54:30 +0100

    added js file with code for flipping project cards on index page

---
+++ index.js
@@ -0,0 +1,14 @@
+// EXTERNAL CODE from CodePel: The main function is from CodePel. I added the forEach to get the function to work on all project-cards
+
+//returns a nodelist
+const projectCards = document.querySelectorAll('.project-card');
+console.log(projectCards);
+
+//add eventListener for each projectCard using forEach
+projectCards.forEach(function(projectCard){
+    projectCard.addEventListener('click', function(){
+        projectCard.classList.toggle("is-flipped");
+        console.log(projectCard);
+        console.log('test!');
+    });
+})
